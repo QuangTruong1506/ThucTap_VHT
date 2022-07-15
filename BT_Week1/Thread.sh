3948c51b7878a3c76e84f9513e248aac958a1782
Quang Truong <quangtruongnguyen1506@gmail.com>
2022-07-15 21:54:11 +0700

    Upload Exercise With Histogram and Report

--- BT_Week1/Thread.sh
+++ BT_Week1/Thread.sh
@@ -2,8 +2,8 @@
 
 gcc -pthread -o Thread Thread.c
 
-for period in 1000000 100000 10000 1000 100
+for period in 100
 do
     echo "$period" > freq.txt
-    timeout 60 ./Thread 
+    timeout 30 ./Thread 
 done
